096b01bac9e906d80fa1ed93bdd0b37159abbd61
Ibtisam Mirza <iuhmirza@gmail.com>
2022-09-22 14:59:01 +0100

    Add middleware for token validation

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const middleware = require('./utils/middleware')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 const blogsRouter = require('./controllers/blogs')
@@ -11,6 +12,7 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.tokenExtractor)
 
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', {username: 1, name:1})
@@ -9,6 +10,12 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
+
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if(!decodedToken.id) {
+        return response.status(401).json({error:'token missing or invalid'})
+    }
+    const user = await User.findById(decodedToken.id)
     
     if(!body.url || !body.title) {
         response.status(400).end()
@@ -16,16 +23,13 @@ blogsRouter.post('/', async (request, response) => {
     }
     if(!body.likes) body.likes = 0
 
-    const firstUser = (await User.find({}))[0]
-
     const blog = new Blog({
         ...body,
-        user: firstUser
+        user: user._id
     })
     const savedBlog = await blog.save()
-
-    firstUser.blogs = firstUser.blogs.concat(savedBlog._id)
-    await firstUser.save()
+    user.notes = user.notes.concat(savedBlog._id)
+    await user.save()
 
     response.status(201).json(savedBlog)
 })
